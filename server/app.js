a8a8dcdeb86daaa0ba0cf2a28f340278e3c59ddc
Rebecca Norén <rebecca.noren@iteam.se>
2020-05-11 10:18:12 +0200

    first commit

--- server/app.js
+++ server/app.js
@@ -8,21 +8,31 @@ const logger = require("morgan");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 
 /**
  * Middlewares
  */
+app.use(
+  cors({
+    origin: process.env.FRONT_END_URL,
+  })
+);
 
 app.use(logger("dev")); // This logs HTTP reponses in the console.
 app.use(express.json()); // Access data sent as json @req.body
-app.use(express.urlencoded({ extended: false })); // Access data sent as urlEncoded (standard form or postman) @req.body
+app.use(express.urlencoded({
+  extended: false
+})); // Access data sent as urlEncoded (standard form or postman) @req.body
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app;
+
 app.use(
   session({
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    }),
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
@@ -31,7 +41,7 @@ app.use(
 
 // Test to see if user is logged In before getting into any router.
 app.use(function (req, res, next) {
-  console.log(req.session.currentUser);
+  // console.log(req.session.currentUser);
   next();
 });
 
