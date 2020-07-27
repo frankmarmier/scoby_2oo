dfda5306348107842df1792aa5295bba24d300c4
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-27 15:53:05 +0200

    Add item model and seed

---
+++ server/bin/seed.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+require("../config/dbConnection");
+
+const Item = require("../models/Items");
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+
+async function seedIt() {
+  try {
+    const contact = [
+      {
+        phoneNumber: "0678905436",
+      },
+    ];
+
+    const contactSeed = await Contact.create(contact);
+
+    const user = {
+      firstName: "Audrey",
+      lastName: "Fake",
+      profileImg: "",
+      email: "audreyfake@gmail.com",
+      password: "1234",
+      city: "Paris",
+      contact: contactSeed._id,
+    };
+
+    const userSeed = await User.create(user);
+
+    const item = {
+      name: "Ficus Elastica",
+      description: "Ceci est un figuier",
+      image:
+        "https://emova-monceaufleurs-fr-storage.omn.proximis.com/Imagestorage/images/740/740/5ec2b404e1b81_ficuselastica4.jpg",
+      category: "Plant",
+      quantity: 5,
+      address: "Copenhagen",
+      location: {
+        type: "Point",
+        coordinates: [12.550343, 55.665957],
+        formattedAddress: "Copenhagen",
+      },
+      id_user: userSeed._id,
+    };
+
+    const itemSeed = await Item.create(item);
+
+    console.log("the seed was created", contactSeed, userSeed, itemSeed);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+seedIt();
