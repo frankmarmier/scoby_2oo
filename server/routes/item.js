3161a1918844f8f4df619d049b44095bf6024529
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-27 17:39:18 +0200

    Add item routes

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,13 +1,13 @@
 var express = require("express");
-var router = new express.Router();
+var router = express.Router();
 const itemModel = require("./../models/Items");
-const userModel = require("./../models/Users");
 
 // Get all the items in the DB
 
-router.get((req, res, next) => {
+router.get("/", (req, res, next) => {
   itemModel
     .find()
+    .populate("User")
     .then((items) => {
       res.status(200).json(items);
     })
@@ -16,33 +16,47 @@ router.get((req, res, next) => {
 
 // Get one item in the DB
 
-router.get((req, res, next) => {
+router.get("/:id", (req, res, next) => {
   itemModel
     .findById(req.params.id)
+    .populate("User")
     .then((item) => res.status(200).json(item))
     .catch(next);
 });
 
 // Post one item in the DB
 
-router.post((req, res, next) => {
+router.post("/", (req, res, next) => {
   itemModel
-  .create(req.body)
-  .then((newItem) => {
-    res.status(201).json(newItem)
+    .create(req.body)
+    .then((newItem) => res.status(201).json(newItem))
     .catch(next);
-  });
 });
 
 // Update item in the DB
 
-router.patch((req, res, next) => {
+router.patch("/:id", (req, res, next) => {
+    
   itemModel
-    .findByIdAndUpdate(req.params.id)
+    .findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .populate("User")
     .then((updatedItem) => {
       res.status(200).json(updatedItem);
     })
     .catch(next);
+});
+
+// Delete item in the DB
+
+router.delete("/:id", (req, res, next) => {
+  itemModel
+    .findByIdAndRemove(req.params.id)
+    .then((deletedItem) => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
