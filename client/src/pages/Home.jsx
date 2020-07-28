3b059516337b0afd990d61df0b069885bbb3cb7b
abWasHere <audrey.belson@gmail.com>
2020-07-28 09:25:31 +0200

    shows map , adds stylesheet for map

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,62 @@
 import React from "react";
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import { useState } from "react";
+import apiHandler from "./../api/apiHandler";
 
+const Map = ReactMapboxGl({
+	accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+});
 
-const Home = (props) => {
-  // Implement react map box here.
-  const Map = ReactMapboxGl({
-		accessToken:
-			"pk.eyJ1IjoiYWJ3YXNoZXJlIiwiYSI6ImNrZDRtdWVrMjAxeDIyd255d2QwNmN4OTEifQ.nh5gIz7Wp-z4Cx7Kizf1qw",
-	});
+class Home extends React.Component {
+	state = {
+		longitude:2.3522,
+		latitude:48.8566,
+		allItems: [],
+	};
+	// get coordinate from API
 
+	componentDidMount() {
+		console.log("DANS APIHANDLER");
 
-  return (
-		<div>
-			<h1></h1>
-			<Map
-				style="mapbox://styles/mapbox/streets-v9"
-				containerStyle={{
-					height: "90vh",
-					width: "90vw",
-          margin: "auto"
-				}}
-			>
-				<Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-					<Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-				</Layer>
-			</Map>
-		</div>
-	);
-};
+		apiHandler
+			.getItems()
+			.then((dbRes) => console.log("DB RES ", dbRes))
+			.catch((error) => console.log(error));
+	}
+
+	// Implement react map box here.
+
+	render() {
+		console.log(this.state.longitude, this.state.latitude);
+		return (
+			<div className="mapContainer">
+				<h1>The map</h1>
+				<Map
+					className="mapContainer"
+					style="mapbox://styles/mapbox/streets-v11"
+					containerStyle={{
+						height: "90vh",
+						width: "90vw",
+						margin: "auto",
+						zoom: 1,
+					}}
+					center={[this.state.longitude, this.state.latitude]}
+				>
+		{/* 			{apiData.map((item) => {
+						<Layer
+						key = {item._id}
+							type="symbol"
+							id="marker"
+							layout={{ "icon-image": "marker-15" }}
+						>
+							<Feature coordinates={[item.longitude, item.latitude]} />;
+						</Layer>;
+					})} */}
+				</Map>
+				;
+			</div>
+		);
+	}
+}
 
 export default Home;
