7900ac8d706eda8e0e006811caad85216847545e
Elisa Chen <skechen@gmail.com>
2021-01-11 14:40:20 +0100

    small updated to fix item delete route back

--- server/routes/item.js
+++ server/routes/item.js
@@ -4,58 +4,56 @@ const Item = require("../models/Item");
 
 //GET ALL ITEMS  IN DB
 router.get("/", (req, res, next) => {
-    Item.find()
-        .then((items) => {
-            res.status(200).json(items);
-        })
-        .catch((error) => {
-            next(error)
-        })
+  Item.find()
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 // GET ONE ITEM IN DB
 router.get("/:id", (req, res, next) => {
-    Item.findById(req.params.id)
-        .then((item) => {
-            res.status(200).json(item)
-        })
-        .catch((error) => {
-            next(error)
-        })
+  Item.findById(req.params.id)
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.patch("/:id", (req, res, next) => {
-    Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then((item) => {
-            res.status(200).json(item);
-        })
-        .catch((error) => {
-            next(error)
-        })
+  Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-
 
 router.post("/", (req, res, next) => {
-    Item.create(req.body)
-        .then((item) => {
-            res.status(201).json(item);
-        })
-        .catch((error) => {
-            next(error)
-        })
+  Item.create(req.body)
+    .then((item) => {
+      res.status(201).json(item);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-
-
 
 router.delete("/:id", (req, res, next) => {
-    Item.findByIdAndDelete(req.params.id)
-        .then((item) => {
-            res.send("deleted !")
-        })
-        .catch((error) => {
-            next(error)
-        })
+  Item.findByIdAndDelete(req.params.id)
+    .then((item) => {
+      res.status(204).json({
+        message: "Successfuly deleted",
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-
 
 module.exports = router;
