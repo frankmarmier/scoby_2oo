0b7b35050082c4e4d29d7fc877749b34c647e4aa
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-01-11 14:23:44 +0100

    routes dones

--- server/routes/item.js
+++ server/routes/item.js
@@ -2,26 +2,61 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../models/Item");
 
-
+//GET ALL ITEMS  IN DB
 router.get("/", (req, res, next) => {
-    res.send("All items in DB");
+    Item.find()
+        .then((items) => {
+            res.status(200).json(items);
+        })
+        .catch((error) => {
+            next(error)
+        })
 });
 
-
+// GET ONE ITEM IN DB
 router.get("/:id", (req, res, next) => {
-    res.send("One item in DB");
-});
-
-router.post("/", (req, res, next) => {
-    res.send("create one item in DB");
+    Item.findById(req.params.id)
+        .then((item) => {
+            res.status(200).json(item)
+        })
+        .catch((error) => {
+            next(error)
+        })
 });
 
 router.patch("/:id", (req, res, next) => {
-    res.send("update one item in DB");
+    Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((item) => {
+            res.status(200).json(item);
+        })
+        .catch((error) => {
+            next(error)
+        })
 });
 
+
+router.post("/", (req, res, next) => {
+    Item.create(req.body)
+        .then((item) => {
+            res.status(201).json(item);
+        })
+        .catch((error) => {
+            next(error)
+        })
+});
+
+
+
 router.delete("/:id", (req, res, next) => {
-    res.send("delete one item in DB");
+    Item.findByIdAndDelete(req.params.id)
+        .then((item) => {
+            res.status(204).json({
+                message: "Successfuly deleted",
+            })
+        })
+        .catch((error) => {
+            next(error)
+        })
 });
 
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,11 +3,24 @@ const router = express.Router();
 const User = require("../models/User")
 
 router.get("/me", function (req, res, next) {
-  res.send("get info of current user");
+  User.findById(req.session.currentUser)
+    .select("-password")
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
 });
 
 router.patch("/me", function (req, res, next) {
-  res.send("update current user");
+  User.findByIdAndUpdate(req.session.currentUser, req.body, { new: true })
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((error) => {
+      next(error)
+    })
 });
 
 
